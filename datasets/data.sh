423017c262cef3ae21b52421c9ebff8888370243
why <894321963@163.com>
2021-08-05 16:17:18 +0800

    Delete data.sh

--- datasets/data.sh
+++
@@ -1,4 +0,0 @@
-python3 1_split_filter.py /kaggle/working/FewX/datasets/coco/
-#python3 2_balance.py /kaggle/working/FewX/datasets/coco/
-python3 3_gen_support_pool.py /kaggle/working/FewX/datasets/coco/
-python3 4_gen_support_pool_10_shot.py /kaggle/working/FewX/datasets/coco/
