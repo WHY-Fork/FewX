d8fa186cddc5e187e8ab76d744c08745e217fbb3
why <894321963@163.com>
2021-08-05 16:16:30 +0800

    Update data.sh

--- datasets/data.sh
+++ datasets/data.sh
@@ -1,4 +1,4 @@
-python3 1_split_filter.py ./ 
-#python3 2_balance.py ./
-python3 3_gen_support_pool.py ./
-python3 4_gen_support_pool_10_shot.py ./
+python3 1_split_filter.py /kaggle/working/FewX/datasets/coco/
+#python3 2_balance.py /kaggle/working/FewX/datasets/coco/
+python3 3_gen_support_pool.py /kaggle/working/FewX/datasets/coco/
+python3 4_gen_support_pool_10_shot.py /kaggle/working/FewX/datasets/coco/
