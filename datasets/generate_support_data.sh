a75934e91024de7e7ec29605789bbe667d2d7b50
why <894321963@163.com>
2021-08-05 16:17:55 +0800

    Update generate_support_data.sh

--- datasets/generate_support_data.sh
+++ datasets/generate_support_data.sh
@@ -6,3 +6,7 @@ ln -s $DATA_ROOT/train2017 ./
 ln -s $DATA_ROOT/val2017 ./
 ln -s $DATA_ROOT/annotations ./
 
+python3 1_split_filter.py /kaggle/input/coco2017
+#python3 2_balance.py /kaggle/input/coco2017
+python3 3_gen_support_pool.py /kaggle/input/coco2017
+python3 4_gen_support_pool_10_shot.py /kaggle/input/coco2017
