cc426434170f1c990b67c85c7b407a724be23a83
why <894321963@163.com>
2021-08-05 16:28:36 +0800

    Update generate_support_data.sh

--- datasets/generate_support_data.sh
+++ datasets/generate_support_data.sh
@@ -1,4 +1,4 @@
-DATA_ROOT=/kaggle/input/coco2017
+DATA_ROOT=/kaggle/input/coco-2017-dataset/coco2017
 
 cd coco
 
@@ -6,7 +6,7 @@ ln -s $DATA_ROOT/train2017 ./
 ln -s $DATA_ROOT/val2017 ./
 ln -s $DATA_ROOT/annotations ./
 
-python3 1_split_filter.py /kaggle/input/coco2017
-#python3 2_balance.py /kaggle/input/coco2017
-python3 3_gen_support_pool.py /kaggle/input/coco2017
-python3 4_gen_support_pool_10_shot.py /kaggle/input/coco2017
+python3 1_split_filter.py ./
+#python3 2_balance.py ./
+python3 3_gen_support_pool.py ./
+python3 4_gen_support_pool_10_shot.py ./
